d99ef8691b7d67fc54be5beb9467c56c881cd59f
StefanoPicozzi <StefanoPicozzi@gmail.com>
2016-12-12 07:26:08 +1100

    Update items-create.sql

--- etc/items-create.sql
+++ etc/items-create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE items (
   id int(10) NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
   theme varchar(50) NOT NULL,
+  name varchar(50) NOT NULL,
   caption varchar(50) NOT NULL,
   rank int(10) NOT NULL,
   trivia text NOT NULL,

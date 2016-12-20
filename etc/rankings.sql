eab63ec3e4fd8f417f5a3775adcd1c277075ccbe
Stefano Picozzi <StefanoPicozzi@gmail.com>
2016-12-20 16:07:29 +1100

    Update rankings.sql

--- etc/rankings.sql
+++ etc/rankings.sql
@@ -1,5 +1,12 @@
 
-SELECT name, SUM(rating) AS total 
-FROM ratings
+SELECT i.id AS id, i.name AS name, SUM(r.rating) AS total 
+FROM ratings r, items i
+WHERE r.name = i.name
 GROUP BY name
+UNION
+SELECT id AS id, name AS name, 0 AS total
+FROM items
+WHERE name NOT IN
+(SELECT DISTINCT name
+ FROM ratings)
 ORDER BY total DESC, id ASC;

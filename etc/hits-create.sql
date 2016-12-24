e012bca2276904d59837215e6db911c200b79eb9
Stefano Picozzi <StefanoPicozzi@gmail.com>
2016-12-24 20:53:41 +1030

    x

--- etc/hits-create.sql
+++ etc/hits-create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE hits (
   id INT(10) NOT NULL AUTO_INCREMENT,
-  sessionid VARCHAR(50) NOT NULL,
-  theme VARCHAR(50) NOT NULL,
+  sessionid VARCHAR(50) NULL,
+  theme VARCHAR(50) NULL,
+  clientip VARCHAR(50) NULL,
   created TIMESTAMP,
   PRIMARY KEY (`id`)
 );

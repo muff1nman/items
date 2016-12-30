21785aa6920eaa8fe3b6154118a48942ed20871a
Gogs <gogs@fake.local>
2016-12-30 11:01:46 +1030

    x

--- etc/hits-create.sql
+++ etc/hits-create.sql
@@ -2,6 +2,7 @@ CREATE TABLE hits (
   id INT(10) NOT NULL AUTO_INCREMENT,
   sessionid VARCHAR(50) NULL,
   theme VARCHAR(50) NULL,
+  name VARCHAR(50) NULL,
   clientip VARCHAR(50) NULL,
   created TIMESTAMP,
   PRIMARY KEY (`id`)
